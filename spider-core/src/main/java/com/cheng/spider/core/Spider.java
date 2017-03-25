4c345ac8d37e3a0e5cf7c15fa29d31324de9f21f
chengzhx76 <chengzhx76@qq.com>
2017-03-26 01:24:59 +0800

    nn

--- spider-core/src/main/java/com/cheng/spider/core/Spider.java
+++ spider-core/src/main/java/com/cheng/spider/core/Spider.java
@@ -1,5 +1,15 @@
 package com.cheng.spider.core;
 
+import com.cheng.spider.core.downloader.Downloader;
+import com.cheng.spider.core.downloader.HttpClientDownloader;
+import com.cheng.spider.core.pipeline.Pipeline;
+import com.cheng.spider.core.processor.PageProcessor;
+import com.cheng.spider.core.scheduler.QuenueScheduler;
+import com.cheng.spider.core.scheduler.Scheduler;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Desc:
  * Author: 光灿
@@ -11,6 +21,44 @@ public class Spider implements Task {
 
     private String uuid;
 
+    private List<String> startUrls;
+
+    private Downloader downloader = new HttpClientDownloader();
+
+    private List<Pipeline> pipelines = new ArrayList<>();
+
+    private Scheduler scheduler = new QuenueScheduler();
+
+    private PageProcessor processor;
+
+    /**
+     * 使用已定义抽取规则新建一个爬虫
+     * @param processor 已自定抽取义规则
+     */
+    public Spider(PageProcessor processor) {
+        this.processor = processor;
+        this.site = processor.getSite();
+        this.startUrls = processor.getSite().getStartUrls();
+    }
+    /**
+     * 使用已定义抽取规则新建一个爬虫
+     * @param processor 已自定抽取义规则
+     * @return 新建的爬虫
+     */
+    public static Spider create(PageProcessor processor) {
+        return new Spider(processor);
+    }
+
+    /**
+     * 重新设置startUrls，会覆盖Site本身的startUrls
+     * @param startUrls 开始爬取的地址
+     * @return 新建的爬虫
+     */
+    public Spider startUrls(List<String> startUrls) {
+        this.startUrls = startUrls;
+        return this;
+    }
+
     /**
      * 为爬虫设置一个唯一ID，用于标志任务，默认情况下使用domain作为uuid，对于单domain多任务的情况，请为重复任务设置不同的ID。
      * @param uuid 唯一ID
@@ -20,6 +68,37 @@ public class Spider implements Task {
         this.uuid = uuid;
         return this;
     }
+
+    /**
+     * 设置调度器。调度器用于保存待抓取URL，并可以进行去重、同步、持久化等工作。默认情况下使用内存中的阻塞队列进行调度。
+     * @param scheduler 调度器
+     * @return this
+     */
+    public Spider scheduler(Scheduler scheduler) {
+        this.scheduler = scheduler;
+        return this;
+    }
+
+    /**
+     * 设置处理管道。处理管道用于最终抽取结果的后处理，例如：保存到文件、保存到数据库等。默认情况下会输出到控制台。
+     * @param pipeline 处理管道
+     * @return this
+     */
+    public Spider pipeline(Pipeline pipeline) {
+        this.pipelines.add(pipeline);
+        return this;
+    }
+
+    /**
+     * 设置下载器。处理下载页面的请求。默认情况下使用HttpClient。
+     * @param downloader 下载器
+     * @return this
+     */
+    public Spider downloader(Downloader downloader) {
+        this.downloader = downloader;
+        return this;
+    }
+
 
     @Override
     public Site getSite() {
---
+++ spider-core/src/main/java/com/cheng/spider/core/downloader/HttpClientDownloader.java
@@ -0,0 +1,17 @@
+package com.cheng.spider.core.downloader;
+
+import com.cheng.spider.core.Page;
+import com.cheng.spider.core.Request;
+import com.cheng.spider.core.Spider;
+
+/**
+ * Desc:
+ * Author: 光灿
+ * Date: 2017/3/26
+ */
+public class HttpClientDownloader implements Downloader {
+    @Override
+    public Page download(Request request, Spider spider) {
+        return null;
+    }
+}
